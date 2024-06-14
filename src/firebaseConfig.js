10e6903516da44f782fb44cd7224ee1a21e4001e
monish237 <monish.muralicharan@gmail.com>
2024-06-14 17:59:00 +0530

    secured api key

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,20 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getFirestore, disableNetwork } from "firebase/firestore";
+import { firebaseConfig } from '../config';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBOrAASRxHDMP3XByLHuitE3tLMEHT9KXo",
-  authDomain: "adsgpt-5c8b4.firebaseapp.com",
-  projectId: "adsgpt-5c8b4",
-  storageBucket: "adsgpt-5c8b4.appspot.com",
-  messagingSenderId: "204518708474",
-  appId: "1:204518708474:web:7e3f4475af8dcf6d91e543",
-  measurementId: "G-1SZ4ZM51JN"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
