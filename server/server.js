26aed6c6ae62755cd17d10228f50a32add13b3e4
monish237 <monish.muralicharan@gmail.com>
2024-06-19 19:39:09 +0530

    another change for heroku

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ import { addCreator } from '../src/scripts/addCreator.js';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
